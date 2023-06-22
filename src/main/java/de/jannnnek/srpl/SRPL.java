e8d344141934e6ad2861bb95e383c1972a0ca5a4
JannekHas <JannekHas@live.de>
2023-06-23 00:44:52 +0200

    Upload

---
+++ src/main/java/de/jannnnek/srpl/SRPL.java
@@ -0,0 +1,52 @@
+package de.jannnnek.srpl;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerResourcePackStatusEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.awt.*;
+
+public class SRPL extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        if (getConfig().getString("resource-pack") == null || getConfig().getString("resource-pack-sha1") == null) {
+            return;
+        }
+        e.getPlayer().setResourcePack(getConfig().getString("resource-pack"), getConfig().getString("resource-pack-sha1"));
+    }
+
+    @EventHandler
+    public void onResourcePack(PlayerResourcePackStatusEvent e) {
+        if (getConfig().get("force-pack") == null) {
+            return;
+        }
+        if (getConfig().getBoolean("force-pack")) {
+            Player p = e.getPlayer();
+            if (e.getStatus().equals(PlayerResourcePackStatusEvent.Status.DECLINED) || e.getStatus().equals(PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD)) {
+                if (getConfig().get("kick-message-on-decline") == null) {
+                    p.kick();
+                } else {
+                    p.kickPlayer(getConfig().getString("kick-message-on-decline"));
+                }
+            }
+        }
+    }
+}
